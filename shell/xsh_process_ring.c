bb68a9716987fc269a67e7cb70d79966b9a05017
hushi <hushi@indiana.edu>
2016-09-09 12:15:42 -0400

    assignment 2

---
+++ shell/xsh_process_ring.c
@@ -0,0 +1,19 @@
+/* process_ring file
+ * created by Shichao Hu
+ * created on 9/9/2016
+ * part of assignment2 */
+
+#include <stdio.h>
+#include <string.h>
+#include <xinu.h>
+
+shellcmd process_ring(int nargs, char *args[]){
+	int countdown;
+	int proc;
+	int round;
+	if(){
+		printf("Ring Element %d" + " : " + "Round %d" + " : " + "Value : %d\n");
+		proc++;
+		
+	}
+}
